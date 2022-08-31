8e86e5b6405b46a8bdea78e4ba9a6bddc3dde4d3
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-08-31 10:46:35 -0300

    Add initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <div>
+            <header>
+                <h1>Sample Page</h1>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
